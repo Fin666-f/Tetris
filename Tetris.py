3ae8053d773aef1358a42cb4511f7e977e1bc57a
user <alexandrafuntikova4833@gmail.com>
2024-01-15 17:20:38 +0300

    Реализация клетчатого поля и окна подсчитывания очков Фунтиковой Александрой.

--- Tetris.py
+++ Tetris.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pygame.init()
+size = width, height = 1920, 1080
+screen = pygame.display.set_mode(size)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def render(screen):
+    #Клетчатое поле
+    s = 53
+    for i in range(20):
+        y = 10 + s * i
+        for j in range(10):
+            x = 695 + s * j
+            pygame.draw.rect(screen, pygame.Color(255, 255, 255), (x, y, s, s), 1)
+    pygame.draw.rect(screen, pygame.Color(255, 255, 255), (20, 20, s * 6, s * 4), 3)
+    #Подсчёт очков
+    font = pygame.font.Font(None, 90)
+    text = font.render('SCORE:', True, (255, 255, 255))
+    screen.blit(text, (60, 60))
+    count = 0
+    text = font.render(f'{count}', True, (255, 255, 255))
+    screen.blit(text, (60, 150))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    render(screen)
+    pygame.display.flip()
+
+pygame.quit()
